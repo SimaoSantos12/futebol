dfa6287996c9d105ad456154193ae66dcd13aa54
Diogo Santos <diogocostasantos1@outlook.pt>
2025-09-28 22:33:32 +0100

    inserir dos clubes

--- db_scripts/20250928_diogo.sql
+++ db_scripts/20250928_diogo.sql
@@ -2,6 +2,8 @@
 
 --      CRIAR UMA BASE DE DADOS COM O NOME futebol
 
+--      É UM EXEMPLO DO QUE SE PODE FAZER
+
 --/////////////////////////////////////////////////////////
 
 
@@ -40,13 +42,17 @@ SELECT setval('tipo_posicao_gid_seq', (SELECT MAX(gid) FROM tipo_posicao));
 
 CREATE TABLE jogador_futebol (
     gid SERIAL PRIMARY KEY,
-    nome VARCHAR(150) NOT NULL,
+    denominacao VARCHAR(150) NOT NULL,
     id_tipo_posicao VARCHAR(50),
     createdon DATETIME DEFAULT CURRENT_TIMESTAMP,
-    updateon DATETIME DEFAULT CURRENT_TIMESTAMP
+    updateon DATETIME DEFAULT CURRENT_TIMESTAMP,
+    FOREIGN KEY (id_tipo_posicao)
+        REFERENCES tipo_posicao(gid)
+        ON DELETE NO ACTION
+        ON UPDATE CASCADE
 );
 
-INSERT INTO jogador_futebol (nome, id_tipo_posicao) VALUES
+INSERT INTO jogador_futebol (denominacao, id_tipo_posicao) VALUES
 ('Nicolás Otamendi', 2),
 ('Bruma', 10),
 ('Fredrik Aursnes', 6),
@@ -79,3 +85,34 @@ INSERT INTO jogador_futebol (nome, id_tipo_posicao) VALUES
 ('Leandro', 2),
 ('Gonçalo Sobral', 1);
 
+CREATE TABLE clube_futebol (
+    gid SERIAL PRIMARY KEY,
+    denominacao VARCHAR(100) NOT NULL,
+    cidade VARCHAR(100),
+    pais VARCHAR(100),
+    funcadao DATE,
+    estado VARCHAR(1) NOT NULL DEFAULT 'A' CHECK (estado IN ('A', 'Y', 'Z')),
+    createdon TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    updateon TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
+
+INSERT INTO clube_futebol (denominacao, cidade, pais, funcadao) VALUES
+('SL Benfica', 'Lisboa', 'Portugal', '1904-02-28'),
+('FC Porto', 'Porto', 'Portugal', '1893-09-28'),
+('Sporting CP', 'Lisboa', 'Portugal', '1906-07-01'),
+('SC Braga', 'Braga', 'Portugal', '1921-01-19'),
+('Vitória SC', 'Guimarães', 'Portugal', '1922-09-22'),
+('Boavista FC', 'Porto', 'Portugal', '1903-08-01'),
+('FC Famalicão', 'Vila Nova de Famalicão', 'Portugal', '1931-08-21'),
+('Rio Ave FC', 'Vila do Conde', 'Portugal', '1939-05-10'),
+('GD Estoril Praia', 'Estoril', 'Portugal', '1939-05-17'),
+('Casa Pia AC', 'Lisboa', 'Portugal', '1920-07-03'),
+('Moreirense FC', 'Moreira de Cónegos', 'Portugal', '1938-11-01'),
+('FC Vizela', 'Vizela', 'Portugal', '1939-01-01'),
+('Gil Vicente FC', 'Barcelos', 'Portugal', '1924-05-03'),
+('Portimonense SC', 'Portimão', 'Portugal', '1914-08-14'),
+('FC Arouca', 'Arouca', 'Portugal', '1951-12-25'),
+('Estrela da Amadora', 'Amadora', 'Portugal', '1932-01-01'),
+('AVS Futebol SAD', 'Vila das Aves', 'Portugal', '1930-01-01'),
+('FC Alverca', 'Alverca do Ribatejo', 'Portugal', '1939-01-01'),
+('CD Nacional', 'Funchal', 'Portugal', '1910-12-08');
